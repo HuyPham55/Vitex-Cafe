b367e3ff19cf11acfaee442abfd3789697cd870e
Huy Pham <huyducphamofficial@gmail.com>
2026-03-16 18:51:28 +0700

    Removed old folder, preparing for submodule

--- api-server/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { login, initAdmin } = require('../controllers/authController');
-
-router.post('/login', login);
-router.post('/init', initAdmin);
-
-module.exports = router;

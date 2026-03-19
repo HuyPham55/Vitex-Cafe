753280459f21ba3bab723d91447beeba326f94d6
Huy Pham <huyducphamofficial@gmail.com>
2026-03-19 16:30:50 +0700

    Remove api-server folder to convert to submodule

--- api-server/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getReviews, createReview } = require('../controllers/reviewController');
-const { imageUpload } = require('../middleware/uploadMiddleware');
-
-router.get('/:productId', getReviews);
-router.post('/', imageUpload.array('photos', 5), createReview);
-
-module.exports = router;
